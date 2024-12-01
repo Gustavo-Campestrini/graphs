d6859040c9484fc3b6b0ec951f4b453b772bc897
Magalu <lucas.teixeira@luizalabs.com>
2024-12-01 17:13:35 -0300

    feat: coloring graph

--- python/grafos.py
+++ python/grafos.py
@@ -17,8 +17,10 @@ basepath = "/home/magalu/sandbox/graphs/curriculos/"
 
 # inicializa algumas variaveis necessarias para a execucao
 curriculos = []
-pesquisadores = {}
+todos_pesquisadores = {}
 todos_autores = {}
+todos_orientados_mestrado = {}
+todos_orientados_doutorado = {}
 id_cont = 1
 
 
@@ -39,20 +41,26 @@ def remove_acentos(texto: str) -> str:
     return only_ascii.decode(encoding="iso-8859-1")
 
 
-def inicia_autores_curriculos(curriculos: list[dict]) -> None:
-    """
-    Insere os pesquisadores dos currículos fornecidos no dicionário 'todos_autores'.
-    Essa inserção é feita para registrar o nome fornecido nos currículos, o que depois facilita a inserção de vértices com cores
-    diferentes, que permitem destacar esses pesquisadores.
+def normaliza_nome(nome: str) -> str:
+    nome = remove_acentos(nome).lower().replace("-", " ")
+    if "," in nome:
+        nomes = nome.split(",")
+        nome = nomes[1].strip() + " " + nomes[0].strip()
+        
+    return nome
 
-    Args:
-        curriculos (list[str]): Curriculos dos pesquisadores que são base do trabalho.
-    """
-    for curriculo in curriculos:
-        id = curriculo["CURRICULO-VITAE"]["@NUMERO-IDENTIFICADOR"]
-        nome = remove_acentos(curriculo["CURRICULO-VITAE"]["DADOS-GERAIS"]["@NOME-COMPLETO"]).lower()
-        primeira_citacao = remove_acentos(curriculo["CURRICULO-VITAE"]["DADOS-GERAIS"]["@NOME-EM-CITACOES-BIBLIOGRAFICAS"]).split(";")[0]
-        todos_autores[primeira_citacao] = {"id": id, "nome": nome}
+
+def existe_na_base(id: str, nome: str, nomes_citacao: list[str], base: dict):
+    for k, v in base.items():
+        nomes = nome.split()
+        nome = nomes[0]
+        sobrenome = nomes[-1]
+        pattern = f"{nome} ([a-z]+ )*{sobrenome}( [a-z]+ ?)*"
+        nomes_citacao_em_comum = len(set(v["nomes_citacao"]).intersection(set(nomes_citacao)))
+        if k == nome or re.search(pattern, k) or nomes_citacao_em_comum > 0 or v["id"] == id:
+            return k, True
+        
+    return "", False
 
 
 def busca_ou_registra_autor(autor: dict) -> dict:
@@ -60,38 +68,34 @@ def busca_ou_registra_autor(autor: dict) -> dict:
     Procura o autor dentro do dicionário 'todos_autores', se baseando em seus nomes para citação, seu ID e seu nome.
     Como os nomes podem possuir várias variações, são feitas algumas verificações (que não garantem a não duplicidade do mesmo indivíduo).
     Caso o autor não seja encontrado, ele é inserido no dicionário.
-    Por fim, os dados do autor são retornados pela função.
+    Por fim, o nome do autor são retornados pela função.
 
     Args:
         autor (str): Autor para ser procurado ou registrado.
 
     Returns:
-        dict (dict of str: str): Dados do autor.
+        str: Nome do autor.
     """
     global id_cont
     id = autor["@NRO-ID-CNPQ"]
-    nome_autor = remove_acentos(autor["@NOME-COMPLETO-DO-AUTOR"]).lower().replace("-", " ")
-    if "," in nome_autor:
-        nomes = nome_autor.split(",")
-        nome_autor = nomes[1].strip() + " " + nomes[0].strip()
-        
+    nome_autor = normaliza_nome(autor["@NOME-COMPLETO-DO-AUTOR"])
     nomes_citacao = remove_acentos(autor["@NOME-PARA-CITACAO"]).replace("-", " ").split(";")
-    for k, v in todos_autores.items():
-        nomes = nome_autor.split()
-        nome = nomes[0]
-        sobrenome = nomes[-1]
-        pattern = f"{nome} ([a-z]+ )*{sobrenome}( [a-z]+ ?)*"
-        if k in nomes_citacao or v["nome"] == nome_autor or v["id"] == id or re.search(pattern, v["nome"]):
-            return {"id": v["id"], "nome": v["nome"]}
-    else:
-        id = autor["@NRO-ID-CNPQ"]
-        if id == "":
-            id = str(id_cont)
-            id_cont += 1
+    k, ok = existe_na_base(id, nome_autor, nomes_citacao, todos_pesquisadores)
+    if ok:
+        return k
+    
+    k, ok = existe_na_base(id, nome_autor, nomes_citacao, todos_autores)
+    if ok:
+        return k
+    
+    id = autor["@NRO-ID-CNPQ"]
+    if id == "":
+        id = str(id_cont)
+        id_cont += 1
 
-        autor_obj = {"id": id, "nome": nome_autor} 
-        todos_autores[nomes_citacao[0]] = autor_obj
-        return autor_obj
+    autor_obj = {"id": id, "nomes_citacao": nomes_citacao} 
+    todos_autores[nome_autor] = autor_obj
+    return nome_autor
             
 
 def formata_autores(items: list[dict] | dict) -> list[dict]:
@@ -102,7 +106,7 @@ def formata_autores(items: list[dict] | dict) -> list[dict]:
         items (list(dict) | dict): Um ou mais autores para serem formatados
 
     Returns:
-        list(dict): Autores formatos com informações reduzidas e padronizadas.
+        list(dict): Nomes formatados dos autores.
     """
     if isinstance(items, dict):
         items = [items]
@@ -147,81 +151,154 @@ def formata_orientacoes(items: list[dict] | dict, tipo: Literal["MESTRADO", "DOU
         list(dict): Orientações formatos com informações reduzidas e padronizadas.
     """
     if isinstance(items, dict):
-        obj = [{
-            "titulo": items[f"DADOS-BASICOS-DE-ORIENTACOES-CONCLUIDAS-PARA-{tipo}"]["@TITULO"],
-            "orientado": items[f"DETALHAMENTO-DE-ORIENTACOES-CONCLUIDAS-PARA-{tipo}"]["@NOME-DO-ORIENTADO"]
-        }]
-        return obj
+        items = [items]
     
     orientacoes = []
     for orientacao in items:
+        titulo = orientacao[f"DADOS-BASICOS-DE-ORIENTACOES-CONCLUIDAS-PARA-{tipo}"]["@TITULO"]
+        nome = normaliza_nome(orientacao[f"DETALHAMENTO-DE-ORIENTACOES-CONCLUIDAS-PARA-{tipo}"]["@NOME-DO-ORIENTADO"])
         orientacoes.append(
             {
-                "titulo": orientacao[f"DADOS-BASICOS-DE-ORIENTACOES-CONCLUIDAS-PARA-{tipo}"]["@TITULO"],
-                "orientado": orientacao[f"DETALHAMENTO-DE-ORIENTACOES-CONCLUIDAS-PARA-{tipo}"]["@NOME-DO-ORIENTADO"]
+                "titulo": titulo,
+                "orientado": nome,
             }
         )
 
+        if tipo == "MESTRADO":
+            todos_orientados_mestrado[nome] = True
+        else:
+            todos_orientados_doutorado[nome] = True
+        
     return orientacoes
 
 
+def inicia_pesquisadores(curriculos: list[dict]) -> None:
+    """
+    Insere os pesquisadores dos currículos fornecidos no dicionário 'todos_autores'.
+    Essa inserção é feita para registrar o nome fornecido nos currículos, o que depois facilita a inserção de vértices com cores
+    diferentes, que permitem destacar esses pesquisadores.
+
+    Args:
+        curriculos (list[str]): Curriculos dos pesquisadores que são base do trabalho.
+    """
+    for curriculo in curriculos:
+        id = curriculo["CURRICULO-VITAE"]["@NUMERO-IDENTIFICADOR"]
+        nome = normaliza_nome(curriculo["CURRICULO-VITAE"]["DADOS-GERAIS"]["@NOME-COMPLETO"])
+        nomes_citacao = remove_acentos(curriculo["CURRICULO-VITAE"]["DADOS-GERAIS"]["@NOME-EM-CITACOES-BIBLIOGRAFICAS"]).split(";")
+        todos_pesquisadores[nome] = {
+            "id": id,
+            "nomes_citacao": nomes_citacao,
+            "artigos": [],
+            "orientandos": {
+                "mestrado": [],
+                "doutorado": [],
+            },
+        }
+
+        artigos = list(filter(lambda x: x["DADOS-BASICOS-DO-ARTIGO"]["@NATUREZA"] == "COMPLETO", curriculo["CURRICULO-VITAE"]["PRODUCAO-BIBLIOGRAFICA"]["ARTIGOS-PUBLICADOS"]["ARTIGO-PUBLICADO"]))
+        todos_pesquisadores[nome]["artigos"] = formata_artigos(artigos)
+
+        if (orientacoes := curriculo["CURRICULO-VITAE"]["OUTRA-PRODUCAO"]["ORIENTACOES-CONCLUIDAS"].get("ORIENTACOES-CONCLUIDAS-PARA-MESTRADO", False)):
+            todos_pesquisadores[nome]["orientandos"]["mestrado"] = formata_orientacoes(orientacoes, "MESTRADO")
+        
+        if (orientacoes := curriculo["CURRICULO-VITAE"]["OUTRA-PRODUCAO"]["ORIENTACOES-CONCLUIDAS"].get("ORIENTACOES-CONCLUIDAS-PARA-DOUTORADO", False)):
+            todos_pesquisadores[nome]["orientandos"]["doutorado"] = formata_orientacoes(orientacoes, "DOUTORADO")
+
+
 for filename in os.listdir(basepath):
-    with open(basepath + filename, "r+", encoding="iso-8859-1") as file:
-        curriculos.append(xmltodict.parse(file.read(), encoding="iso-8859-1"))
+    if filename == "2060996038464074.xml":
+        with open(basepath + filename, "r+", encoding="iso-8859-1") as file:
+            curriculos.append(xmltodict.parse(file.read(), encoding="iso-8859-1"))
 
-inicia_autores_curriculos(curriculos)
-
-for curriculo in curriculos:
-    id = curriculo["CURRICULO-VITAE"]["@NUMERO-IDENTIFICADOR"]
-    nome = remove_acentos(curriculo["CURRICULO-VITAE"]["DADOS-GERAIS"]["@NOME-COMPLETO"]).lower()
-    primeira_citacao = remove_acentos(curriculo["CURRICULO-VITAE"]["DADOS-GERAIS"]["@NOME-EM-CITACOES-BIBLIOGRAFICAS"]).split(";")[0]
-    pesquisadores[id] = {
-        "nome": nome,
-        "artigos": [],
-        "orientandos": {
-            "mestrado": [],
-            "doutorado": [],
-        },
-    }
-
-    artigos = list(filter(lambda x: x["DADOS-BASICOS-DO-ARTIGO"]["@NATUREZA"] == "COMPLETO", curriculo["CURRICULO-VITAE"]["PRODUCAO-BIBLIOGRAFICA"]["ARTIGOS-PUBLICADOS"]["ARTIGO-PUBLICADO"]))
-    pesquisadores[id]["artigos"] = formata_artigos(artigos)
-
-    if (orientacoes := curriculo["CURRICULO-VITAE"]["OUTRA-PRODUCAO"]["ORIENTACOES-CONCLUIDAS"].get("ORIENTACOES-CONCLUIDAS-PARA-MESTRADO", False)):
-        pesquisadores[id]["orientandos"]["mestrado"] = formata_orientacoes(orientacoes, "MESTRADO")
-    
-    if (orientacoes := curriculo["CURRICULO-VITAE"]["OUTRA-PRODUCAO"]["ORIENTACOES-CONCLUIDAS"].get("ORIENTACOES-CONCLUIDAS-PARA-DOUTORADO", False)):
-        pesquisadores[id]["orientandos"]["doutorado"] = formata_orientacoes(orientacoes, "DOUTORADO")
+inicia_pesquisadores(curriculos)
 
 g = nx.Graph()
 
-color_map = []
 label_list = {}
 
-for k, v in pesquisadores.items():
-    g.add_node(v["nome"])
-    color_map.append("yellow")
-    label_list[v["nome"]] = v["nome"]
+pesquisadores_node_list = []
+outros_individuos_node_list = []
 
-for k, v in pesquisadores.items():
+# adiciona pesquisadores como nodes
+for k, v in todos_pesquisadores.items():
+    g.add_node(k)
+    pesquisadores_node_list.append(k)
+    label_list[k] = k
+
+# adiciona autores como nodes
+for k, v in todos_pesquisadores.items():
     for a in v["artigos"]:
         for i in a["autores"]:
             for j in a["autores"]:
-                if i["nome"] == j["nome"]:
+                if i == j:
                     continue
 
-                if not g.has_node(i["nome"]):                  
-                    g.add_node(i["nome"], weight=1)
-                    color_map.append("blue")
+                if not g.has_node(i):                  
+                    g.add_node(i, weight=1)
+                    outros_individuos_node_list.append(i)
 
-                if not g.has_node(j["nome"]):                  
-                    g.add_node(j["nome"], weight=1)
-                    color_map.append("green")
+                if not g.has_node(j):                  
+                    g.add_node(j, weight=1)
+                    outros_individuos_node_list.append(j)
 
-                g.add_edge(i["nome"], j["nome"])
+                g.add_edge(i, j)
 
+# adiciona orientandos como nodes
+for k, v in todos_pesquisadores.items():
+    for orientacao in v["orientandos"]["mestrado"]:
+        if not g.has_node(orientacao["orientado"]):
+            g.add_node(orientacao["orientado"], weight=1)
+            outros_individuos_node_list.append(orientacao["orientado"])
+            g.add_edge(k, orientacao["orientado"])
+
+    for orientacao in v["orientandos"]["doutorado"]:
+        if not g.has_node(orientacao["orientado"]):
+            g.add_node(orientacao["orientado"], weight=1)
+            outros_individuos_node_list.append(orientacao["orientado"])
+            g.add_edge(k, orientacao["orientado"])
+
+print("COAUTORES:", todos_autores.keys())
+
+node_colors = []
+
+for node in g.nodes:
+    is_pesquisador = node in todos_pesquisadores.keys()
+    if is_pesquisador:
+        continue
+
+    is_coautor = node in todos_autores.keys()
+    is_mestrado = node in todos_orientados_mestrado.keys()
+    is_doutorado = node in todos_orientados_doutorado.keys()
+
+    # Definir a cor com base nas condições
+    if is_coautor and is_mestrado and is_doutorado:
+        node_colors.append("orange")  # Coautor, mestrado e doutorado
+    elif is_coautor and is_mestrado:
+        node_colors.append("purple")  # Coautor e mestrado
+    elif is_coautor and is_doutorado:
+        node_colors.append("green")  # Coautor e doutorado
+    elif is_mestrado and is_doutorado:
+        node_colors.append("brown")  # Mestrado e doutorado
+    elif is_coautor:
+        node_colors.append("blue")  # Apenas coautor
+    elif is_mestrado:
+        node_colors.append("red")  # Apenas mestrado
+    elif is_doutorado:
+        node_colors.append("yellow")  # Apenas doutorado
+
+#label_list = {node: node for node in g.nodes}
 pos = nx.kamada_kawai_layout(g, scale=2, dim=2)
-nx.draw_networkx_nodes(g, pos, node_size=50, node_color=color_map)
+nx.draw_networkx_nodes(g, pos, node_size=100, nodelist=pesquisadores_node_list, node_color="gray")
+nx.draw_networkx_nodes(g, pos, node_size=50, nodelist=outros_individuos_node_list, node_color=node_colors)
 nx.draw_networkx_edges(g, pos)
 _ = nx.draw_networkx_labels(g, pos, labels=label_list, font_color="red")
 plt.show()
+
+# pesquisador analisado = cinza
+# co-autores = azul
+# mestrado = vermelho
+# doutorado = amarelo
+# co-autores && mestrado = roxo
+# co-autores && doutorado = verde
+# mestrado && doutorado = marrom
+# co-autores && doutorado && mestrado = laranja
---
+++ python/teste.py
@@ -0,0 +1,5 @@
+a = [True, True, True, False, True]
+
+aa = [1 for i in a if i == True]
+
+print(aa)
